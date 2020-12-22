50e2dbf4b67b0849ee40f2b1fe2f53129aa95051
Jhon Gesell Villanueva Portella <unmsm182@gmail.com>
2020-12-22 12:02:41 -0500

    novedades aunque no corre el script

---
+++ JG/funcs/backUteis/license.py
@@ -0,0 +1,12 @@
+class Data_company():
+    def dados(self):
+        self.NomeEmpresa = "Mecánica de prueba"
+        self.NomeRuaEmp = "Calle 123"
+        self.NomeBairroEmp = "Brasil"
+        self.MunicipioEmp = "Ciudad - Uf"
+        self.TelefoneEmp = "Teléfono: 99 9999 9999"
+        self.Licenca = "Licencia abierta!!!"
+        self.LicencaCpf     = ""
+        #self.versãoLicenca = 'Open'
+        self.versãoLicenca = 'Custom'
+
